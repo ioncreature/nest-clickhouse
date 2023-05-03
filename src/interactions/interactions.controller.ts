dc24f930c953ae94bb3d3aaf480c1de75862421d
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2023-05-03 15:02:15 +0200

    Added SQL examples for create and insert

--- src/interactions/interactions.controller.ts
+++ src/interactions/interactions.controller.ts
@@ -17,3 +17,61 @@ export class InteractionsController {
     return 'ok';
   }
 }
+
+/*
+CREATE TABLE IF NOT EXISTS dtu.rx_reports (
+  "ctag" String,
+  "date_time" DateTime64(3, 'UTC'),
+  "element" String,
+  "element_path" Array(String),
+  "element_type" String,
+  "event_type" String,
+  "page_title" String,
+  "topic" String,
+  "uid" String,
+  "url_domain_name" String,
+  "url_parameters" String,
+  "url_path" String,
+  "url_port" String,
+  "url_scheme" String,
+  "value" String
+)
+ENGINE = MergeTree
+ORDER BY "date_time";
+
+
+INSERT INTO dtu.rx_reports (
+  "ctag", 
+  "date_time", 
+  "element", 
+  "element_path", 
+  "element_type", 
+  "event_type", 
+  "page_title", 
+  "topic", 
+  "uid", 
+  "url_domain_name", 
+  "url_parameters", 
+  "url_path", 
+  "url_port", 
+  "url_scheme", 
+  "value"
+) VALUES (
+  'DEMO MVP',
+  1681863554176,
+  '5 min',
+  ['', 'Show data for', '5 min'],
+  'anchor',
+  'click',
+  'Demo account for "Do They Use" Product Intelligence app',
+  'your real usage now',
+  'you@example.com',
+  '',
+  '',
+  '/index.html',
+  '',
+  'file:',
+  '5 min'
+);
+
+*/
