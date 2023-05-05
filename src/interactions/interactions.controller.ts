1d7c91428b5e0760a466cad9142fcd8bdf42972c
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2023-05-05 13:44:26 +0200

    Added SELECT statements examples

--- src/interactions/interactions.controller.ts
+++ src/interactions/interactions.controller.ts
@@ -74,4 +74,31 @@ INSERT INTO dtu.rx_reports (
   '5 min'
 );
 
+
+// 3 types of SELECT statements are used.
+// Number of "AND" params in each SELECT statement is variable and is given from user_filters = {...} dict:
+// for (let key in user_filters) {
+//    select += 'AND ' + key + ' = ' + user_filters[key]
+// }
+// If Node.js provides built-in mechanisms for SQL injection prevention please apply them
+// If not, we will need to make something like: https://www.stackhawk.com/blog/node-js-sql-injection-guide-examples-and-prevention
+// (the same for INSERT statements, now they are vulnerable)
+
+// 1. Simple SELECT:
+SELECT * FROM dtu.rx_reports WHERE 1=1
+AND ctag = 'DEMO MVP'
+AND topic = 'your real usage now'
+AND date_time >= 1681863554 
+AND date_time < 1681863555;
+
+// 2. Aggregation with count:
+SELECT uid, COUNT(uid) as count FROM dtu.rx_reports WHERE 1=1
+AND ctag = 'DEMO MVP'
+AND topic = 'your real usage now'
+GROUP BY uid;
+
+// 3. SELECT distinct values:
+SELECT DISTINCT topic FROM dtu.rx_reports WHERE 1=1
+AND ctag = 'DEMO MVP' 
+ORDER BY ctag ASC;
 */
