bb88d5f7ab485962fad60a8d2e13349084e0a686
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2023-05-09 20:57:11 +0200

    Supported filtering by element_path_string

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -57,21 +57,13 @@ export class ApiService {
     let whereClause = `SELECT DISTINCT ${something_distinct} FROM dtu.rx_data WHERE `
     whereClause += `ctag = '${query.ctag}' `;
     for (let key in query) {
-      if (!['ctag', 'element_path'].includes(key)) {
+      if (!['ctag', 'element_path', 'element_path_string'].includes(key))
         whereClause += `AND ` + key + ` = '` + query[key] + `' `;
-      }
+      if (key == 'element_path_string')
+        whereClause += `AND ` + key + ` like '` + query[key] + `%' `;
     }
-    //whereClause += `FORMAT JSONCompactColumns`;
-    console.log(whereClause, query);
-    
-    const db_returned_improper_format = await this.clickHouseService.query(whereClause);
-    //let db_returned_proper_format = []; // to JSONCompactColumns https://clickhouse.com/docs/en/interfaces/formats#jsoncompactcolumns
-    //for (let i in db_returned_improper_format)
-    //  db_returned_proper_format.push(db_returned_improper_format[i][something_distinct]);
-    //console.log('got from db:', db_returned_improper_format);
-    //console.log('sent in reply:', db_returned_proper_format);
-    
-    return db_returned_improper_format;
+    //console.log(whereClause, query);
+    return await this.clickHouseService.query(whereClause);;
   }
 
   async readAggregatedApi(query: GetSelectQuery) {
