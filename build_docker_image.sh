5ef2e5b365acc1f061d287f049f0a20c2d92b70e
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2023-05-13 14:42:32 +0200

    Create build_docker_image.sh

---
+++ build_docker_image.sh
@@ -0,0 +1,14 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'Pass tag for a new version. For example:'
+    echo 'build_docker_image.sh 1.0'
+    exit 0
+fi
+
+docker image rm dtu-rc &&
+docker build --no-cache -t dtu-rc . &&
+
+docker tag dtu-rc alexeyhimself/dtu:$1 &&
+docker tag dtu-rc alexeyhimself/dtu:latest &&
+
+docker push alexeyhimself/dtu:$1 &&
+docker push alexeyhimself/dtu:latest
